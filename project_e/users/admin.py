2418455b83b13f311ba5c8f2a2966f6c5db1e888
muzykafs <muzykafs123@gmail.com>
2020-01-23 19:20:57 -0500

    getting scraps

--- project_e/users/admin.py
+++ project_e/users/admin.py
@@ -1,17 +1,57 @@
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from project_e.users.forms import UserChangeForm, UserCreationForm
+
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from .models import GuestEmail
 
 User = get_user_model()
 
 
-@admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
 
-    form = UserChangeForm
-    add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "name", "is_superuser"]
-    search_fields = ["name"]
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'admin')
+    list_filter = ('admin', 'staff', 'active')
+    fieldsets = (
+        (None, {'fields': ('full_name', 'email', 'password')}),
+       ('Full name', {'fields': ()}),
+        ('Permissions', {'fields': ('admin', 'staff', 'active',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email', 'full_name',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+
+# Remove Group Model from admin. We're not using it 
+# but if you want you can add it back any time.
+admin.site.unregister(Group)
+
+
+
+class GuestEmailAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    class Meta:
+        model = GuestEmail
+
+
+admin.site.register(GuestEmail, GuestEmailAdmin)
