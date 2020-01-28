b89c6bda10312f29f107a645a7b5a62d57eadaf4
Willem Drescher <willemdrescher@yahoo.com>
2020-01-28 18:12:20 -0500

    add homepage animations and some styling fixes (#15)

--- config/settings/base.py
+++ config/settings/base.py
@@ -267,7 +267,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "optional"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "project_e.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
