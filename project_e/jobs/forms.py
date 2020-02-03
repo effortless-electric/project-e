90b4f4a4b25d3f83a428df1ac3c28ccce691d6f4
effortless-electric <60072357+effortless-electric@users.noreply.github.com>
2020-02-03 09:14:12 -0500

    improve contract fields and contract creation form (#21)
    Co-authored-by: Willem Drescher <willemdrescher@yahoo.com>

--- project_e/jobs/forms.py
+++ project_e/jobs/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 from project_e.jobs.models import Job
+from django.forms.widgets import Textarea
 
 class ContractForm(forms.Form):
     email = forms.EmailField(label="Email", max_length=100)
     first_name = forms.CharField(label="First Name", max_length=100)
     last_name = forms.CharField(label="Last Name", max_length=100)
+    customer_address = forms.CharField(label="Address", max_length=200)
+    contract_price = forms.IntegerField(label="Cost", max_value=99999, localize=True)
     # phone = PhoneNumberField(label="Phone Number")
     vin = forms.CharField(label="VIN", min_length=17, max_length=17)
+    car_year = forms.IntegerField(label="Year", max_value=9999, localize=True)
     car_make = forms.CharField(label="Make", max_length=100)
     car_model = forms.CharField(label="Model", max_length=100)
-    notes = forms.CharField(label="Notes", max_length=1000)
-   
+    notes = forms.CharField(label="Notes", required=False, max_length=1000, widget=Textarea)
