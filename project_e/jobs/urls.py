0f70cccd8c7a150ad8878227486fec2e8abb6c54
effortless-electric <60072357+effortless-electric@users.noreply.github.com>
2020-02-13 12:43:16 -0500

    Update urls.py

--- project_e/jobs/urls.py
+++ project_e/jobs/urls.py
@@ -10,6 +10,6 @@ app_name = "jobs"
 urlpatterns = [
     path("create/", view=job_creation_view, name="create"),
     path("<int:pk>/", view=job_detail_view, name="detail"),
-    path("pdf/<int:pk>/", view=pdf_contract_detail_view, name="pdf")
+    path("pdf/<int:pk>/", view=pdf_contract_detail_view, name="pdf"),
     path("contacted/<int:pk>/", view=job_customer_contacted, name="contacted")
 ]
